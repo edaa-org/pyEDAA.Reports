24bb6ef5deb9d98b158bdf4afc0a4324fffc736e
Patrick Lehmann <Paebbels@gmail.com>
2023-07-15 22:54:23 +0200

    Bumped dependencies.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 #                                                                                                                      #
 # License:                                                                                                             #
 # ==================================================================================================================== #
-# Copyright 2021-2022 Electronic Design Automation Abstraction (EDA²)                                                  #
+# Copyright 2021-2023 Electronic Design Automation Abstraction (EDA²)                                                  #
 #                                                                                                                      #
 # Licensed under the Apache License, Version 2.0 (the "License");                                                      #
 # you may not use this file except in compliance with the License.                                                     #
