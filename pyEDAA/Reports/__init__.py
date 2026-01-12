e85ace5647cc08c3d3a0dd081bb8b8e706024cf0
Patrick Lehmann <Paebbels@gmail.com>
2026-01-12 01:43:52 +0100

    Bumped copyright information.

--- pyEDAA/Reports/__init__.py
+++ pyEDAA/Reports/__init__.py
@@ -11,7 +11,7 @@
 #                                                                                                                      #
 # License:                                                                                                             #
 # ==================================================================================================================== #
-# Copyright 2021-2025 Electronic Design Automation Abstraction (EDA²)                                                  #
+# Copyright 2021-2026 Electronic Design Automation Abstraction (EDA²)                                                  #
 #                                                                                                                      #
 # Licensed under the Apache License, Version 2.0 (the "License");                                                      #
 # you may not use this file except in compliance with the License.                                                     #
@@ -33,7 +33,7 @@ Various report abstract data models and report format converters.
 """
 __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
-__copyright__ = "2021-2025, Electronic Design Automation Abstraction (EDA²)"
+__copyright__ = "2021-2026, Electronic Design Automation Abstraction (EDA²)"
 __license__ =   "Apache License, Version 2.0"
 __version__ =   "0.17.2"
 __keywords__ =  ["Reports", "Abstract Model", "Data Model", "Unit Testing", "Testcase", "Testsuite", "OSVVM", "YAML", "XML"]
